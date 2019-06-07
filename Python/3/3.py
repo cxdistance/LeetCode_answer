7c4e62a0867352352cb19e6ab2068f12cc16282c
Lu Chenxu <lcxemail73@gmail.com>
2019-06-07 12:57:46 +0800

    Create 3.py

---
+++ Python/3/3.py
@@ -0,0 +1,20 @@
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {}
+        maxlen = 0
+        pre = -1
+        for i in range(len(s)):
+            if s[i] in dic:
+                pre = max(pre, dic[s[i]])
+            maxlen = max(maxlen, i-pre)
+            dic[s[i]] = i
+        return maxlen
+
+
+so = Solution()
+s = '2234'
+length = so.lengthOfLongestSubstring(s)
+print(length)
