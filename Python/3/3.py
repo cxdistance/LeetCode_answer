b40abe53ca8ac6cd4aef78b897978834a2772b34
Lu Chenxu <lcxemail73@gmail.com>
2019-06-07 13:00:48 +0800

    Create 3.py

---
+++ Python/3/3.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {}
+        maxlen = 0
+        pre = -1
+        for i in range(len(s)):
+            if s[i] in dic:
+                pre = max(pre, dic[s[i]])
+            maxlen = max(maxlen, i-pre)
+            dic[s[i]] = i
+        return maxlen
+
+
